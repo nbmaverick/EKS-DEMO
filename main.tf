06027ad91d6692785ffc9c81e4a849c8629b5e95
“nbmaverick” <devops3036@gmail.com>
2024-04-28 22:32:13 +0000

    added node

--- main.tf
+++ main.tf
@@ -1,23 +1,61 @@
-# resource "aws_eks_cluster" "myeks" {
-#   name     = "myeks"
-#   role_arn = aws_iam_role.eks-role.arn
+# EKS Cluster
 
-#   version = "1.27"
+resource "aws_eks_cluster" "myeks" {
+  name     = "myeks"
+  role_arn = aws_iam_role.eks-role.arn
+
+  version = "1.27"
 
 
-#   vpc_config {
-#     endpoint_public_access = true
-#     subnet_ids = [
-#       aws_subnet.public-us-west-2a.id,
-#       aws_subnet.public-us-west-2b.id
-#     ]
-#   }
-#   depends_on = [
-#     aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
-#     aws_iam_role.eks-role
-#   ]
+  vpc_config {
+    endpoint_public_access = true
+    subnet_ids = [
+      aws_default_subnet.default_az1.id,
+      aws_default_subnet.default_az2.id,
+      aws_default_subnet.default_az3.id
+    ]
+  }
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
+    aws_iam_role.eks-role
+  ]
 
-#   tags = {
-#     Name = "myeks"
-#   }
-# }
+  tags = {
+    Name = "myeks"
+  }
+}
+
+# EKS Node 
+
+resource "aws_eks_node_group" "my-nodes" {
+  cluster_name    = aws_eks_cluster.myeks.name
+  node_group_name = "my-nodes"
+  node_role_arn   = aws_iam_role.node-role.arn
+
+  subnet_ids = [
+      aws_default_subnet.default_az1.id,
+      aws_default_subnet.default_az2.id,
+      aws_default_subnet.default_az3.id
+  ]
+
+  version = "1.27"
+
+  capacity_type  = "ON_DEMAND"
+  instance_types = ["t3.medium"]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.nodes-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.nodes-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.nodes-AmazonEC2ContainerRegistryReadOnly
+  ]
+
+  tags = {
+    Name = "my-nodes"
+  }
+}
--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-2"
 }
 
-# provider "kubernetes" {
-#   config_path    = "~/.kube/config"
-#   config_context = "my-context"
-# }
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "my-context"
+}
