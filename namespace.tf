567c864655c029499d58d607244ff6d20350d4a2
“nbmaverick” <devops3036@gmail.com>
2024-04-30 02:54:47 +0000

    added prometheus

--- namespace.tf
+++ namespace.tf
@@ -4,3 +4,16 @@
 #     name = "grafana"
 #   }
 # }
+resource "time_sleep" "wait_for_kubernetes" {
+  depends_on = [
+    aws_eks_cluster.myeks
+  ]
+  create_duration = "20s"
+}
+
+resource "kubernetes_namespace" "prometheus" {
+  depends_on = [aws_eks_node_group.my-nodes, time_sleep.wait_for_kubernetes]
+  metadata {
+    name = "prometheus"
+  }
+}
